0a83becf27a3175fb710cf6ac8d52a67e6dec052
Luong Trong Dai <ltdai91@gmail.com>
2014-11-11 06:41:30 +0700

    add $watch to wishListCount

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,7 +11,18 @@ angular.module('myApp.controllers', [])
 
 angular.module('myApp.controllers')
 		.controller('BookController', function($scope){
+			$scope.wishListCount = 0;
 			$scope.books = ['Jump Start HTML5', 'Jump Start CSS', 'Jump Start Responsive Web Design'];
 			$scope.name = 'Scope for BookController';
+
+			$scope.addToWishList = function (book) {
+				$scope.wishListCount++;
+			}
+			$scope.$watch('wishListCount', function(newValue, oldValue) {
+				console.log('Called ' + newValue + ' times.');
+				if (newValue == 2) {
+					alert('Great! You have 2 items in you wish list. Time to buy what you love.');
+				}
+			});
 		});
 
