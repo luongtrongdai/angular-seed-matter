bd5b6be59e5984a387ba73b76668fc5182de972a
Luong Trong Dai <ltdai91@gmail.com>
2014-11-14 06:50:23 +0700

    Create event

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,10 @@
 'use strict';
 
 /* Controllers */
+angular.module('myApp.controller', []);
 
-angular.module('myApp.controllers', [])
+
+angular.module('myApp.controllers')
 		.controller('SiteController', function($scope){
 			$scope.publisher = "SitePoint";
 			$scope.type = "Web Development";
@@ -28,7 +30,7 @@ angular.module('myApp.controllers')
 
 		});
 
-angular.module('myApp.controllers', [])
+angular.module('myApp.controllers')
 		.controller('TimeoutController', function($scope, $timeout){
 			$scope.scheduleTask = function () {
 				$timeout(function() {
@@ -36,4 +38,31 @@ angular.module('myApp.controllers', [])
 					console.log('message='+$scope.message);
 				}, 3000);
 			}
+		});
+
+/* Event in action */
+angular.module('myApp.controller')
+		.controller('MessageController', function($scope, $timeout){
+			$scope.messages = [{
+				sender: 'user1'
+				, text: "Message 1"
+			}];
+			var timer;
+			var count = 0;
+			$scope.loadMessage = function () {
+				count++;
+				$scope.messages.push({
+					sender: "user1"
+					, text: "Random message " + count
+				});
+				timer = $timeout($scope.loadMessages, 2000);
+				if (count == 3) {
+					$scope.$broadcast('EVENT_NO_DATA', 'Not Conneccted');
+					$timeout.cancel(timer);
+				}
+			};
+			timer = $timeout($scope.loadMessages, 2000);
+			$scope.$on('EVENT_RECEIVED', function () {
+				console.log('Received emitted event');
+			});
 		});
