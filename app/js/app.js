95ebf7f7165d7066a85dad56635e0f5a1604e43a
luongtrongdai <11123048@st.hcmuaf.edu.vn>
2014-11-17 14:57:11 +0700

    add ui-router

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,28 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.controllers']);
+angular.module('myApp', ['myApp.controllers', 'ui.router']);
 
 angular.module('myApp').run(function($rootScope){
 	$rootScope.title = "Timeout controller";
 	$rootScope.name = "Root Scrope";
+});
+
+angular.module('myApp').config(function($stateProvider, $urlRouterProvider,$locationProvider){
+    $stateProvider.state('view1',{
+        url: '/view1',
+        controller:'Controller1',
+        templateUrl:'/partials/view1.html'
+    }).state('view2',{
+        url: '/view2/:firstname/:lastname',
+        controller:'Controller2',
+        resolve:{
+            names: function(){
+                return ['Misko','Vojta','Brad'];
+            }
+        },
+        templateUrl: '/partials/view2.html'
+    });
+    $urlRouterProvider.otherwise('/view1'); // when no route match found redirect to /view1
+    $locationProvider.html5Mode(true);
 });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -79,3 +79,20 @@ angular.module('myApp.controllers')
 				$scope.$emit('EVENT_RECEIVED');
 			});
 		});
+
+angular.module('myApp.controllers')
+		.controller('Controller1', function ($scope, $location, $state) {
+			$scope.loadView2 = function () {
+				$state.go('view2', {
+					firstname: $scope.firstname
+					, lastname: $scope.lastname
+				});
+			};
+		});
+
+angular.module('myApp.controllers')
+		.controller('Controller2', function ($scope, $stateParams, names) {
+			$scope.firstname = $stateParams.firstname;
+			$scope.lastname = $stateParams.lastname;
+			$scope.names = names;
+		});
