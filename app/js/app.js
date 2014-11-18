77029f5bb26655b2b8c303f14244a84a80e62d29
Luong Trong Dai <ltdai91@gmail.com>
2014-11-19 01:37:19 +0700

    demo merge

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,21 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.controllers']);
+angular.module('myApp', ['myApp.controllers', 'ngRoute']);
 
 angular.module('myApp').run(function($rootScope){
 	$rootScope.title = "Timeout controller";
 	$rootScope.name = "Root Scrope";
+});
+
+angular.module('myApp').config(function($routeProvider, $locationProvider) {
+	$routeProvider.when('/view1', {
+		controller: 'Controller1'
+		, templateUrl: '/partials/view1.html'
+	}).when('/view2/:firstname/:lastname', {
+		controller: 'Controller2'
+		, templateUrl: '/partials/view2.html'
+	}).otherwise({redirectTo: '/view1'});
+
+	$locationProvider.html5Mode(true);	//Active HTML5 mode
 });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -79,3 +79,16 @@ angular.module('myApp.controllers')
 				$scope.$emit('EVENT_RECEIVED');
 			});
 		});
+
+angular.module('myApp.controllers')
+		.controller('Controller1', function($scope, $location){
+			$scope.loadView2 = function () {
+				$location.path('/view2/' + $scope.firstname + '/' + $scope.lastname);
+			};
+		});
+
+angular.module('myApp.controllers')
+		.controller('Controller2', function($scope, $routeParams){
+			$scope.firstname = $routeParams.firstname;
+			$scope.lastname = $routeParams.lastname;
+		});
